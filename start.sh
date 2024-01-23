cca53122ac379ed836e05226309df1e0f1cda8e5
Saksham <saksham3939@gmail.com>
2024-01-24 00:07:37 +0530

    Fix attempt for glitch server start script

--- start.sh
+++ start.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 # this file is meant to be used by glitch for initializing server
-pip3 install poetry
-poetry install
-poetry shell
-gunicorn main:app -b 0.0.0.0:3000
+pip3 install -r requirements.txt
+python3 -m gunicorn main:app -b 0.0.0.0:3000
